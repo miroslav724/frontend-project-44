ba11f7ee48ec4ad02d6cde4d403ac97e3609db3e
miroslav724 <ulanovmiroslav6@gmail.com>
2024-10-10 11:20:55 +0300

    add brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,41 @@
+import readline from 'readline-sync';
+
+const isEven = (number) => {
+    return number % 2 === 0;
+};
+
+const Greeting = () => {
+    console.log('Welcome to the Brain Games!');
+
+    const name = readline.question('May I have your name? ');
+    console.log(`Hello, ${name}!`)
+
+    console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+    let correctAnswers = 0;
+
+    const askQuestion = () => {
+     if (correctAnswers === 3) {
+     console.log(`Congratulations, ${name}!`);
+      return;
+      }
+
+     const number = Math.floor(Math.random() * 100) + 1;
+     console.log(`Question: ${number}`);
+     const answer = readline.question('Your answer: ');
+
+     if (answer.toLowerCase() === 'yes' && isEven(number)) {
+        console.log('Correct!');
+        correctAnswers++;
+     } else if (answer.toLowerCase() === 'no' && !isEven(number)) {
+        console.log('Correct!');
+        correctAnswers++;
+     } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(number) ? 'yes' : 'no'}'.\nLets try again, ${name}`)
+     }
+     askQuestion();
+   };
+   askQuestion();
+};
+
+Greeting();
