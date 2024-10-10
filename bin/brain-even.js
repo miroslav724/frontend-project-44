4024998d5d761d9af22dd181e04f01e540efdfb2
miroslav724 <ulanovmiroslav6@gmail.com>
2024-10-10 18:31:15 +0300

    add shebang & inf in Makefile etc

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,41 +1,41 @@
+#!/usr/bin/env node
+
 import readline from 'readline-sync';
 
-const isEven = (number) => {
-    return number % 2 === 0;
-};
+const isEven = (number) => number % 2 === 0;
 
 const Greeting = () => {
-    console.log('Welcome to the Brain Games!');
+  console.log('Welcome to the Brain Games!');
 
-    const name = readline.question('May I have your name? ');
-    console.log(`Hello, ${name}!`)
+  const name = readline.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    let correctAnswers = 0;
+  let correctAnswers = 0;
 
-    const askQuestion = () => {
-     if (correctAnswers === 3) {
-     console.log(`Congratulations, ${name}!`);
+  const askQuestion = () => {
+    if (correctAnswers === 3) {
+      console.log(`Congratulations, ${name}!`);
       return;
-      }
+    }
 
-     const number = Math.floor(Math.random() * 100) + 1;
-     console.log(`Question: ${number}`);
-     const answer = readline.question('Your answer: ');
+    const number = Math.floor(Math.random() * 100) + 1;
+    console.log(`Question: ${number}`);
+    const answer = readline.question('Your answer: ');
 
-     if (answer.toLowerCase() === 'yes' && isEven(number)) {
-        console.log('Correct!');
-        correctAnswers++;
-     } else if (answer.toLowerCase() === 'no' && !isEven(number)) {
-        console.log('Correct!');
-        correctAnswers++;
-     } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(number) ? 'yes' : 'no'}'.\nLets try again, ${name}`)
-     }
-     askQuestion();
-   };
-   askQuestion();
+    if (answer.toLowerCase() === 'yes' && isEven(number)) {
+      console.log('Correct!');
+      correctAnswers += 1;
+    } else if (answer.toLowerCase() === 'no' && !isEven(number)) {
+      console.log('Correct!');
+      correctAnswers += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(number) ? 'yes' : 'no'}'.\nLets try again, ${name}`);
+    }
+    askQuestion();
+  };
+  askQuestion();
 };
 
 Greeting();
